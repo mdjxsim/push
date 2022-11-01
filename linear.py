6398b1c16cb661a74725ac44ffe5e045c4fe1a01
karthi <karthi65.v@gmail.com>
2022-11-01 15:37:13 +0530

    j

---
+++ linear.py
@@ -0,0 +1,7 @@
+def linear search(arr,x):
+    for i in range(len(arr)):
+        if arr[i]==x;
+        return i
+arr=[1,2,3,4,5,6,7,8]
+x=4
+print("element found at index"+str(linear search(arr,x))
