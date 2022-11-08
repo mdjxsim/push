94b35a0ce558a0ae91a8dd3055cbeab6ce599ed1
karthi <karthi65.v@gmail.com>
2022-11-08 13:50:46 +0530

    fg

---
+++ string.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class Exception
+{
+	public static void main(String[] args)
+	{
+		String languages={"c","c++","java","perl","perl","python"};
+		try for(int c=1;c<=5;C++)
+		{
+			System.out.println(language[c]);
+		}
+		catch( Exception e)
+		{
+			System.out.println("exception occurs:ArrayIndexout of Bound Exception");
+		}
+	}
+}			
